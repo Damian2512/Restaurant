1877ad7c857fe2e7ce72ff5df0ef7574cbf74f19
Damian <Damian.berns@student.aventus.nl>
2019-08-08 20:12:26 +0200

    Restaurant website + database

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include('includes/includes.php'); ?>
+</head>
+
+<body>
+
+<!-- Navigatie -->
+<?php include('includes/indexnav.php'); ?>
+
+<!-- Bootstrap javascript in footer -->
+<?php include('includes/footer.php'); ?>
+</body>
+
+</html>
